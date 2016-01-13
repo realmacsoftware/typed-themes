2c9412dfcefb18c231686f27675f2f3f39ed2573
James Beith <james@beith.com>
2016-01-13 20:55:48 +0000

    Add neon theme

---
+++ neon/_src/js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  /*
+     ------------------------------------
+     FitVids
+     ------------------------------------
+  */
+
+  $('.post-excerpt, .post-content').fitVids()
+
+  /*
+     ------------------------------------
+     Navigation
+     ------------------------------------
+  */
+
+  $('.menu-toggle').click(function () {
+    $('.site-navigation').slideToggle(250)
+
+    var centerHeight = $('.site-navigation .center').outerHeight()
+
+    $('.site-navigation .center').css({
+      'margin-top': -(centerHeight/2)
+    })
+  })
+})
