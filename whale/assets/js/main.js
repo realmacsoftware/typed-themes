57761d1d035316c38e411b3af7b0c18f291b435c
James Beith <james@beith.com>
2016-01-13 20:37:24 +0000

    Add whale theme

---
+++ whale/assets/js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  /*
+     ------------------------------------
+     Responsive navigation
+     ------------------------------------
+  */
+
+  var nav = responsiveNav(".site-navigation nav", {
+    label: "",
+    insert: "before",
+    customToggle: "#nav-toggle",
+    openPos: "static"
+  })
+
+  /*
+     ------------------------------------
+     Fitvids
+     ------------------------------------
+  */
+
+  $(".entry-content").fitVids()
+});
+
+
